9377dd1b3449722a1387a160d8eb906ae11b47d9
dani-ribeiro <10DanielRibeiro@gmail.com>
2022-02-07 09:19:11 -0500

    Added eventListeners and functions to number buttons

--- script.js
+++ script.js
@@ -26,3 +26,38 @@ const zero = document.getElementById("zero");
 const point = document.getElementById("point");
 const equals= document.getElementById("equals");
 
+let screenArr = [];     //array where the inputed current operation will appear --> 2 + 2 =
+let resultsArr = [];    //array where the results from screenArr will be sent --> 4
+
+//add click eventListeners to each button
+clearBtn.addEventListener('click', clear);
+seven.addEventListener('click', clickNumber);
+eight.addEventListener('click', clickNumber);
+nine.addEventListener('click', clickNumber);
+four.addEventListener('click', clickNumber);
+five.addEventListener('click', clickNumber);
+six.addEventListener('click', clickNumber);
+one.addEventListener('click', clickNumber);
+two.addEventListener('click', clickNumber);
+three.addEventListener('click', clickNumber);
+zero.addEventListener('click', clickNumber);
+plus.addEventListener('click', clickNumber);
+equals.addEventListener('click', evaluate);
+
+
+
+function clear(){
+    screenArr = [];
+    resultsArr = [];
+    screenText.textContent = "0";
+    console.log("cleared screen");
+}
+
+function clickNumber(){
+    screenArr.push(this.innerHTML);
+}
+
+function evaluate(){
+    screenArr.push(this.innerHTML);
+    
+}
