be7a0728f49ae7fb71fb2da464558489abb8701d
dani-ribeiro <10DanielRibeiro@gmail.com>
2022-02-06 22:54:17 -0500

    Assigned variables to modify html elements through the DOM using script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const screenText = document.getElementById("screenText");
+
+//getting all of the buttons
+//row 1
+const clearBtn = document.getElementById("AC");
+const plusMinus = document.getElementById("plus/minus");
+const percent = document.getElementById("percent");
+const divide = document.getElementById("divide");
+//row 2
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const x = document.getElementById("X");
+//row 3
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const minus = document.getElementById("minus"); 
+//row 4
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const plus = document.getElementById("plus");
+//row 5
+const zero = document.getElementById("zero");
+const point = document.getElementById("point");
+const equals= document.getElementById("equals");
